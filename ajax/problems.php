55f6ecf2dfea0c580428b6848557b757bee04a87
Jun Zheng <junzheng@exchange.niyume.com>
2016-04-27 09:35:49 -0600

    added api

---
+++ ajax/problems.php
@@ -0,0 +1,26 @@
+<?php
+  include "../include/util.php";
+  include "../config.php";
+  include "../include/user_status_ajax.php";
+  include "../include/databaseconnection.php";
+  $dbConnection = new DatabaseConnection($blogDatabaseHost,$blogDatabaseUsername,$blogDatabasePassword,$blogDatabaseName);
+  include "../include/contest.php";
+  $contestStatus = json_decode(file_get_contents("../status.json"),true);
+
+  if($userInfo["group"] != $usernamePrefix . "admin" && $userInfo["group"] != $usernamePrefix . "competitor"){
+    exit; //Not loggedin
+  } else {
+    if($_GET["id"] != 40){
+      echo "problem not found";
+      exit;
+    } else {
+      $problem = getProblem($dbConnection,$_POST["id"]);
+      if($problem){
+        $problem["flag"] = md5($problem["flag"]);
+        echo json_encode($problem);
+      } else {
+        echo "problem not found";
+      }
+    }
+  }
+?>
